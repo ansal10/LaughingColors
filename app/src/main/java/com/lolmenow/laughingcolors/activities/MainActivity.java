69b36a8fea307b988864c830ca9c98f0c1df205b
Anas <anas.ansal10@gmail.com>
2018-07-24 23:45:11 +0530

    adding recyclerview with snaphelper

--- app/src/main/java/com/lolmenow/laughingcolors/activities/MainActivity.java
+++ app/src/main/java/com/lolmenow/laughingcolors/activities/MainActivity.java
@@ -3,7 +3,9 @@ package com.lolmenow.laughingcolors.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PagerSnapHelper;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SnapHelper;
 
 import com.lolmenow.laughingcolors.R;
 import com.lolmenow.laughingcolors.adapters.MainContentAdapter;
@@ -17,11 +19,15 @@ public class MainActivity extends AppCompatActivity {
     public MainContentAdapter mainContentAdapter;
     @BindView(R.id.activity_main_recycler_view_id) public RecyclerView mainContentRecyclerView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        SnapHelper snapHelper = new PagerSnapHelper();
+        snapHelper.attachToRecyclerView(mainContentRecyclerView);
         mainContentAdapter = new MainContentAdapter(Content.dummpyData(), this);
         mainContentRecyclerView.setHasFixedSize(true);
         mainContentRecyclerView.setLayoutManager(new LinearLayoutManager(this));
